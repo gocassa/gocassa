d0bb8494068778d55d5ac33ea8e2de1c728e82f4
crufter <janosdobronszki@citromail.hu>
2014-11-25 16:34:42 +0000

    Reflection blood sugar secs magic

--- interfaces.go
+++ interfaces.go
@@ -10,8 +10,17 @@ type Collection interface {
 	Read(id string) Record // Load record immediately - we need to return error, or lazy load it, which means we need to return errors at later operations
 	MultiRead(ids []string) ([]Record, errors)
 	List(idStart, idEnd string, limit int) ([]Record, errors)
-	Set(id string, interface{}) error // Use for creating a record
+	Set(id string, interface{}) error // Use for creating a record @cruftalert
 }
+
+// A record loaded from a database
+type Record interface {
+	Set(interface{}) // Used for updating a loaded record @cruftalert
+	ToMap() map[string]interface{}
+	ToStruct(i interface{})
+}
+
+// These are just here to not forget about them
 
 type TimeSeriesIndex interface {
 
@@ -19,11 +28,4 @@ type TimeSeriesIndex interface {
 
 type EqualityIndex interface {
 
-}
-
-// A record loaded from a database
-type Record interface {
-	Set(interface{}) // Used for updating a loaded record
-	ToMap() map[string]interface{}
-	ToStruct(i interface{})
 }
---
+++ reflect/reflect.go
@@ -0,0 +1,56 @@
+// This package provides some punk-rock reflection which is not in the stdlib.
+package reflect
+
+import (
+	// "fmt"
+	r "reflect"
+	//"strings"
+)
+
+// Converts a struct to a map.
+func StructToMap(val interface{}) (map[string]interface{}, bool) {
+	// indirect so function works with both structs and pointers to them
+	structVal := r.Indirect(r.ValueOf(val))
+	kind := structVal.Kind()
+	if kind != r.Struct {
+		return nil, false
+	}
+	typ := structVal.Type()
+	mapVal := make(map[string]interface{})
+	for i := 0; i < typ.NumField(); i++ {
+		field := structVal.Field(i)
+		mapVal[typ.Field(i).Name] = field.Interface()
+	}
+	return mapVal, true
+}
+
+// v should be a pointer to a struct.
+func MapToStruct(m map[string]interface{}, struc interface{}) error {
+	val := r.Indirect(r.ValueOf(struc))
+	for k, v := range m {
+		structField := val.FieldByName(k)
+		if structField.Type().Name() == r.TypeOf(v).Name() {
+			structField.Set(r.ValueOf(v))
+		}
+	}
+	return nil
+}
+
+// Nice copypaste bro _,^
+func FieldsAndValues(val interface{}) ([]string, []interface{}, bool) {
+	// indirect so function works with both structs and pointers to them
+	structVal := r.Indirect(r.ValueOf(val))
+	kind := structVal.Kind()
+	if kind != r.Struct {
+		return nil, nil, false
+	}
+	typ := structVal.Type()
+	fields := []string{}
+	values := []interface{}{}
+	for i := 0; i < typ.NumField(); i++ {
+		field := structVal.Field(i)
+		fields = append(fields, typ.Field(i).Name)
+		values = append(values, field.Interface())
+	}
+	return fields, values, true
+}
