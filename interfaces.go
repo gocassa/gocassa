974a0b4ae8ebe8d18f53d1e573a28e4ff0f39f9a
crufter <janosdobronszki@citromail.hu>
2014-11-26 15:35:22 +0000

    Going om way, nuking Record type as per @kiril suggestion

--- interfaces.go
+++ interfaces.go
@@ -8,16 +8,9 @@ type NameSpace interface {
 
 type Collection interface {
 	Read(id string) Record // Load record immediately - we need to return error, or lazy load it, which means we need to return errors at later operations
-	MultiRead(ids []string) ([]Record, errors)
-	List(idStart, idEnd string, limit int) ([]Record, errors)
+	MultiRead(ids []string) ([]interface{}, errors)
+	List(idStart, idEnd string, limit int) ([]interface{}, errors)
 	Set(id string, interface{}) error // Use for creating a record @cruftalert
-}
-
-// A record loaded from a database
-type Record interface {
-	Set(interface{}) // Used for updating a loaded record @cruftalert
-	ToMap() map[string]interface{}
-	ToStruct(i interface{})
 }
 
 // These are just here to not forget about them
