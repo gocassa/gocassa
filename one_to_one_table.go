6fda687f84b4204dc63e5f91883b5e512f009c75
Andreas Garnæs <andreas@users.noreply.github.com>
2015-01-23 17:08:39 +0000

    Simplify multiread.

--- one_to_one_table.go
+++ one_to_one_table.go
@@ -30,9 +30,5 @@ func (o *oneToOneT) Read(id interface{}) (interface{}, error) {
 }
 
 func (o *oneToOneT) MultiRead(ids ...interface{}) ([]interface{}, error) {
-	res, err := o.Where(In(o.idField, ids...)).Query().Read()
-	if err != nil {
-		return nil, err
-	}
-	return res, nil
+	return o.Where(In(o.idField, ids...)).Query().Read()
 }
