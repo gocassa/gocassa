3bbe50de9ee3e72034fe9abb63f9112bee471a37
crufter <janosdobronszki@citromail.hu>
2015-02-10 11:50:35 +0000

    Changes to the interface

--- interfaces.go
+++ interfaces.go
@@ -10,7 +10,6 @@ type Connection interface {
 	CreateKeySpace(name string) error
 	DropKeySpace(name string) error
 	KeySpace(name string) KeySpace
-	Batcher
 }
 
 type KeySpace interface {
@@ -20,41 +19,21 @@ type KeySpace interface {
 	TimeSeriesBTable(tableName, fieldToIndexByField, timeField, uniqueKey string, bucketSize time.Duration, row interface{}) TimeSeriesBTable
 	Table(tableName string, row interface{}, keys Keys) Table
 	DebugMode(bool)
-	Batched
-}
-
-type KeySpaceBatched interface {
-	
 }
 
 //
 // OneToOne recipe
 //
 
-// Simple CRUD interface. You can do CRUD by Id.
-type OneToOneWriter interface {
-	SetWithOptions(v interface{}, opts Options) error
-	Set(v interface{}) error
-	UpdateWithOptions(id interface{}, m map[string]interface{}, opts Options) error
-	Update(id interface{}, m map[string]interface{}) error
-	Delete(id interface{}) error
-}
-
-type OneToOneReader interface {
+type OneToOneTable interface {
+	SetWithOptions(v interface{}, opts Options) WriteOp
+	Set(v interface{}) WriteOp
+	UpdateWithOptions(id interface{}, m map[string]interface{}, opts Options) WriteOp
+	Update(id interface{}, m map[string]interface{}) WriteOp
+	Delete(id interface{}) WriteOp
 	Read(id, pointer interface{}) error
 	MultiRead(ids []interface{}, pointerToASlice interface{}) error
 }
-
-type OneToOneTable interface {
-	OneToOneReader
-	Batched() OneToOneTableBatched
-}
-
-type OneToOneTableBatched interface {
-	OneToOneWriter
-	Batcher
-}
-
 
 //
 // OneToMany recipe
@@ -62,16 +41,15 @@ type OneToOneTableBatched interface {
 
 // OneToMany lets you list rows based on a field equality, eg. 'list all sales where seller id = v'.
 type OneToManyTable interface {
-	SetWithOptions(v interface{}, opts Options) error
-	Set(v interface{}) error
-	UpdateWithOptions(v, id interface{}, m map[string]interface{}, opts Options) error
-	Update(v, id interface{}, m map[string]interface{}) error
-	Delete(v, id interface{}) error
-	DeleteAll(v interface{}) error
+	SetWithOptions(v interface{}, opts Options) WriteOp
+	Set(v interface{}) WriteOp
+	UpdateWithOptions(v, id interface{}, m map[string]interface{}, opts Options) WriteOp
+	Update(v, id interface{}, m map[string]interface{}) WriteOp
+	Delete(v, id interface{}) WriteOp
+	DeleteAll(v interface{}) WriteOp
 	List(v, startId interface{}, limit int, pointerToASlice interface{}) error
 	Read(v, id, pointer interface{}) error
 	MultiRead(id interface{}, ids []interface{}, pointerToASlice interface{}) error
-	Batcher
 	TableChanger
 }
 
@@ -82,14 +60,13 @@ type OneToManyTable interface {
 // TimeSeries lets you list rows which have a field value between two date ranges.
 type TimeSeriesTable interface {
 	// timeField and idField must be present
-	SetWithOptions(v interface{}, opts Options) error
-	Set(v interface{}) error
-	UpdateWithOptions(timeStamp time.Time, id interface{}, m map[string]interface{}, opts Options) error
-	Update(timeStamp time.Time, id interface{}, m map[string]interface{}) error
-	List(start, end time.Time, pointerToASlice interface{}) error
+	SetWithOptions(v interface{}, opts Options) WriteOp
+	Set(v interface{}) WriteOp
+	UpdateWithOptions(timeStamp time.Time, id interface{}, m map[string]interface{}, opts Options) WriteOp
+	Update(timeStamp time.Time, id interface{}, m map[string]interface{}) WriteOp
+	Delete(timeStamp time.Time, id interface{}) WriteOp
 	Read(timeStamp time.Time, id, pointer interface{}) error
-	Delete(timeStamp time.Time, id interface{}) error
-	Batcher
+	List(start, end time.Time, pointerToASlice interface{}) error
 	TableChanger
 }
 
@@ -100,14 +77,13 @@ type TimeSeriesTable interface {
 // TimeSeriesB is a cross between TimeSeries and OneToMany tables.
 type TimeSeriesBTable interface {
 	// timeField and idField must be present
-	SetWithOptions(v interface{}, opts Options) error
-	Set(v interface{}) error
-	UpdateWithOptions(v interface{}, timeStamp time.Time, id interface{}, m map[string]interface{}, opts Options) error
-	Update(v interface{}, timeStamp time.Time, id interface{}, m map[string]interface{}) error
-	List(v interface{}, start, end time.Time, pointerToASlice interface{}) error
+	SetWithOptions(v interface{}, opts Options) WriteOp
+	Set(v interface{}) WriteOp
+	UpdateWithOptions(v interface{}, timeStamp time.Time, id interface{}, m map[string]interface{}, opts Options) WriteOp
+	Update(v interface{}, timeStamp time.Time, id interface{}, m map[string]interface{}) WriteOp
+	Delete(v interface{}, timeStamp time.Time, id interface{}) WriteOp
 	Read(v interface{}, timeStamp time.Time, id, pointer interface{}) error
-	Delete(v interface{}, timeStamp time.Time, id interface{}) error
-	Batcher
+	List(v interface{}, start, end time.Time, pointerToASlice interface{}) error
 	TableChanger
 }
 
@@ -128,9 +104,9 @@ type Filter interface {
 	// Selection modifiers
 	Query() Query
 	// Partial update.
-	Update(m map[string]interface{}) error // Probably this is danger zone (can't be implemented efficiently) on a selectuinb with more than 1 document
-	UpdateWithOptions(m map[string]interface{}, opts Options) error
-	Delete() error
+	Update(m map[string]interface{}) WriteOp // Probably this is danger zone (can't be implemented efficiently) on a selectuinb with more than 1 document
+	UpdateWithOptions(m map[string]interface{}, opts Options) WriteOp
+	Delete() WriteOp
 }
 
 type Keys struct {
@@ -138,9 +114,17 @@ type Keys struct {
 	ClusteringColumns []string
 }
 
-type Batcher interface{
-	Start()
-	Commit() error
+type WriteOp interface{
+	Run() error
+	Add(...WriteOp) WriteOps
+}
+
+type WriteOps interface {
+	Run() error
+	// You do not need this in 95% of the use cases, use Run!
+	// Using Batched writes (logged batch) is very heavy on Cassandra!
+	RunBatched() error
+	Add(...WriteOp) WriteOps
 }
 
 // Danger zone! Do not use this interface unless you really know what you are doing
@@ -155,8 +139,8 @@ type TableChanger interface {
 type Table interface {
 	// Set Inserts, or Replaces your row with the supplied struct. Be aware that what is not in your struct
 	// will be deleted. To only overwrite some of the fields, use Query.Update.
-	Set(v interface{}) error
-	SetWithOptions(v interface{}, opts Options) error
+	Set(v interface{}) WriteOp
+	SetWithOptions(v interface{}, opts Options) WriteOp
 	Where(relations ...Relation) Filter // Because we provide selections
 	TableChanger
 }
@@ -164,4 +148,5 @@ type Table interface {
 type QueryExecutor interface {
 	Query(stmt string, params ...interface{}) ([]map[string]interface{}, error)
 	Execute(stmt string, params ...interface{}) error
+	ExecuteBatched([]stmt string, params [][]interface{}) error
 }
