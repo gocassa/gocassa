0fa25a7da32741b00ccb822e7bb6a5dae6cb75a0
crufter <janosdobronszki@citromail.hu>
2015-03-06 15:03:04 +0000

    Some more tests

--- query_test.go
+++ query_test.go
@@ -79,12 +79,12 @@ func TestMultipleRowResults(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	res := &[]Customer{}
-	err = cs.Where(Eq("Name", "John")).Query().Read(res).Run()
+	res := []Customer{}
+	err = cs.Where(Eq("Name", "John")).Query().Read(&res).Run()
 	if err != nil {
 		t.Fatal(err)
 	}
-	if len(*res) != 2 {
+	if len(res) != 2 {
 		t.Fatal(res)
 	}
 }
@@ -103,16 +103,16 @@ func TestIn(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	res := &[]Customer{}
-	err = cs.Where(In("Id", "100", "200")).Query().Read(res).Run()
-	if len(*res) != 2 {
-		for _, v := range *res {
+	res := []Customer{}
+	err = cs.Where(In("Id", "100", "200")).Query().Read(&res).Run()
+	if len(res) != 2 {
+		for _, v := range res {
 			fmt.Println(v)
 		}
 		t.Fatal("Not found", res)
 	}
-	if (*res)[0].Id != "100" || (*res)[1].Id != "200" {
-		t.Fatal((*res)[0], (*res)[1])
+	if res[0].Id != "100" || res[1].Id != "200" {
+		t.Fatal(res[0], res[1])
 	}
 }
 
@@ -126,20 +126,20 @@ func TestAnd(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	res := &[]Customer{}
-	err = cs.Where(Eq("Id", "100"), Eq("Name", "Joe")).Query().Read(res).Run()
+	res := []Customer{}
+	err = cs.Where(Eq("Id", "100"), Eq("Name", "Joe")).Query().Read(&res).Run()
 	if err != nil {
 		t.Fatal(err)
 	}
-	if len(*res) != 1 {
+	if len(res) != 1 {
 		t.Fatal("Not found ", res)
 	}
 }
 
 func TestQueryReturnError(t *testing.T) {
 	cs := ns.Table("customer2", Customer{}, Keys{})
-	res := &[]Customer{}
-	err := cs.Where(Eq("Id", "100"), Eq("Name", "Joe")).Query().Read(res).Run()
+	res := []Customer{}
+	err := cs.Where(Eq("Id", "100"), Eq("Name", "Joe")).Query().Read(&res).Run()
 	if err == nil {
 		t.Fatal("Table customer2 does not exist - should return error")
 	}
@@ -157,94 +157,81 @@ func TestRowNotFoundError(t *testing.T) {
 }
 
 type Customer3 struct {
-	Id      string
-	Field1  string
-	Field2  int
-	Field3  int32
-	Field4  int64
-	Field5  float32
-	Field6  float64
-	Field7  bool
-	Field8  []byte
-	Field9  []string
-	Field10 []int
-	Field11 []int32
-	Field12 []float32
-	Field13 []float64
-	Field14 []bool
+	Id       string
+	String   string
+	Int      int
+	Int32    int32
+	Int64    int64
+	Float32  float32
+	Float64  float64
+	Bool     bool
+	Bytes    []byte
+	Strings  []string
+	Ints     []int
+	Int32s   []int32
+	Int64s   []int64
+	Float32s []float32
+	Float64s []float64
+	Bools    []bool
+}
+
+func newCustomer3() Customer3 {
+	return Customer3{
+		Id:       "1",
+		String:   "A",
+		Int:      1,
+		Int32:    2,
+		Int64:    3,
+		Float32:  4.0,
+		Float64:  5.0,
+		Bool:     true,
+		Bytes:    []byte{'a', 'b', 'c'},
+		Strings:  []string{"a", "b", "c"},
+		Ints:     []int{1, 2, 3},
+		Int32s:   []int32{1, 2, 3},
+		Int64s:   []int64{1, 2, 3},
+		Float32s: []float32{1.11, 2.22, 3.33},
+		Float64s: []float64{1.11, 2.22, 3.33},
+		Bools:    []bool{false, true, false},
+	}
 }
 
 func TestTypesMarshal(t *testing.T) {
-	c := Customer3{
-		Id:      "1",
-		Field1:  "A",
-		Field2:  1,
-		Field3:  2,
-		Field4:  3,
-		Field5:  4.0,
-		Field6:  5.0,
-		Field7:  true,
-		Field8:  []byte{'a', 'b', 'c'},
-		Field9:  []string{"a", "b", "c"},
-		Field10: []int{1, 2, 3},
-		Field11: []int32{1, 2, 3},
-		Field12: []float32{1.0, 2.0, 3.33},
-		Field13: []float64{1.0, 2.0, 3.33},
-		Field14: []bool{false, true},
-	}
+	c := newCustomer3()
 	tbl := ns.Table("customer3", Customer3{}, Keys{PartitionKeys: []string{"Id"}})
 	createIf(tbl.(TableChanger), t)
 	if err := tbl.Set(c).Run(); err != nil {
 		t.Fatal(err)
 	}
-	res := &[]Customer3{}
-	err := tbl.Where(Eq("Id", "1")).Query().Read(res).Run()
+	res := []Customer3{}
+	err := tbl.Where(Eq("Id", "1")).Query().Read(&res).Run()
 	if err != nil {
 		t.Fatal(err)
 	}
-	if len(*res) != 1 {
+	if len(res) != 1 {
 		t.Fatal(res)
 	}
-	if !reflect.DeepEqual(c, (*res)[0]) {
-		t.Fatal(c, (*res)[0])
+	if !reflect.DeepEqual(c, res[0]) {
+		t.Fatal(c, res[0])
 	}
-}
-
-type Customer4 struct {
-	Id         string
-	FavNums    []int
-	FavStrings []string
 }
 
 func TestUpdateList(t *testing.T) {
-	tbl := ns.Table("customer4", Customer4{}, Keys{PartitionKeys: []string{"Id"}})
+	tbl := ns.Table("customer34", Customer3{}, Keys{PartitionKeys: []string{"Id"}})
 	createIf(tbl.(TableChanger), t)
-	c := Customer4{
-		Id:         "99",
-		FavNums:    []int{1, 2, 3},
-		FavStrings: []string{"yo", "boop", "woot"},
-	}
+	c := newCustomer3()
 	if err := tbl.Set(c).Run(); err != nil {
 		t.Fatal(err)
 	}
-	f := tbl.Where(Eq("Id", "99"))
+	f := tbl.Where(Eq("Id", "1"))
 	err := f.Update(map[string]interface{}{
-		"FavNums": ListRemove(2),
-	}).Add(f.Update(map[string]interface{}{
-		"FavStrings": ListRemove("2"),
-	})).Run()
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = f.Query().ReadOne(&c).Run()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !reflect.DeepEqual(c.FavStrings, []string{"yo", "boop", "woot"}) || !reflect.DeepEqual(c.FavNums, []int{1, 3}) {
-		t.Fatal(c)
-	}
-	err = f.Update(map[string]interface{}{
-		"FavStrings": ListSetAtIndex(1, "changedBoop"),
+		"Strings":  ListRemove("b"),
+		"Ints":     ListRemove(2),
+		"Int32s":   ListRemove(2),
+		"Int64s":   ListRemove(2),
+		"Float32s": ListRemove(2.22),
+		"Float64s": ListRemove(2.22),
+		"Bools":    ListRemove(true),
 	}).Run()
 	if err != nil {
 		t.Fatal(err)
@@ -253,11 +240,17 @@ func TestUpdateList(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if !reflect.DeepEqual(c.FavStrings, []string{"yo", "changedBoop", "woot"}) {
+	if !reflect.DeepEqual(c.Strings, []string{"a", "c"}) ||
+		!reflect.DeepEqual(c.Ints, []int{1, 3}) ||
+		!reflect.DeepEqual(c.Int32s, []int32{1, 3}) ||
+		!reflect.DeepEqual(c.Int64s, []int64{1, 3}) ||
+		!reflect.DeepEqual(c.Float32s, []float32{1.11, 3.33}) ||
+		!reflect.DeepEqual(c.Float64s, []float64{1.11, 3.33}) ||
+		!reflect.DeepEqual(c.Bools, []bool{false, false}) {
 		t.Fatal(c)
 	}
 	err = f.Update(map[string]interface{}{
-		"FavStrings": ListAppend("last"),
+		"Strings": ListSetAtIndex(1, "C"),
 	}).Run()
 	if err != nil {
 		t.Fatal(err)
@@ -266,11 +259,11 @@ func TestUpdateList(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if !reflect.DeepEqual(c.FavStrings, []string{"yo", "changedBoop", "woot", "last"}) {
+	if !reflect.DeepEqual(c.Strings, []string{"a", "C"}) {
 		t.Fatal(c)
 	}
 	err = f.Update(map[string]interface{}{
-		"FavStrings": ListPrepend("first"),
+		"Strings": ListAppend("d"),
 	}).Run()
 	if err != nil {
 		t.Fatal(err)
@@ -279,25 +272,38 @@ func TestUpdateList(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if !reflect.DeepEqual(c.FavStrings, []string{"first", "yo", "changedBoop", "woot", "last"}) {
+	if !reflect.DeepEqual(c.Strings, []string{"a", "C", "d"}) {
+		t.Fatal(c)
+	}
+	err = f.Update(map[string]interface{}{
+		"Strings": ListPrepend("x"),
+	}).Run()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = f.Query().ReadOne(&c).Run()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !reflect.DeepEqual(c.Strings, []string{"x", "a", "C", "d"}) {
 		t.Fatal(c)
 	}
 }
 
 func TestCQLInjection(t *testing.T) {
-	tbl := ns.Table("customer45", Customer4{}, Keys{PartitionKeys: []string{"Id"}})
+	tbl := ns.Table("customer45", Customer3{}, Keys{PartitionKeys: []string{"Id"}})
 	createIf(tbl.(TableChanger), t)
-	c := Customer4{
-		Id:         "99",
-		FavStrings: []string{"yo", "boop", "woot"},
+	c := Customer3{
+		Id:      "1",
+		Strings: []string{"a", "b", "c"},
 	}
 	if err := tbl.Set(c).Run(); err != nil {
 		t.Fatal(err)
 	}
 	// At the moment we don't have batch so we just try to mess up the CQL query with a single quote - if
 	// it can be messed up then we are vulnerable
-	err := tbl.Where(Eq("Id", "99")).Update(map[string]interface{}{
-		"FavStrings": ListRemove("'"),
+	err := tbl.Where(Eq("Id", "1")).Update(map[string]interface{}{
+		"Strings": ListRemove("'"),
 	}).Run()
 	if err != nil {
 		t.Fatal(err)
