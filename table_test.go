d1f7550e6aa0d3477a0bce7b3bf6cda19001b56a
Jeff Jolma <jjolma@gmail.com>
2015-08-11 09:46:14 -0700

    Improve tests
    Test clustering order by reading from Cassandra rather than just
    exampining the create table statement.

--- table_test.go
+++ table_test.go
@@ -54,6 +54,140 @@ func TestCreateTable(t *testing.T) {
 	}
 }
 
+func TestClusteringOrder(t *testing.T) {
+	order := &ClusteringOrder{
+		Columns: []ClusteringOrderColumn{
+			ClusteringOrderColumn{
+				Ascending: false,
+				Column:    "Id",
+			},
+		},
+	}
+	options := Options{Order: order}
+	name := "customer_by_name"
+	cs := ns.Table(name, Customer{}, Keys{
+		PartitionKeys:     []string{"Name"},
+		ClusteringColumns: []string{"Id"},
+	}).WithOptions(options)
+	createIf(cs, t)
+
+	customers := []Customer{
+		Customer{
+			Id:   "1001",
+			Name: "Brian",
+		},
+		Customer{
+			Id:   "1002",
+			Name: "Adam",
+		},
+		Customer{
+			Id:   "1003",
+			Name: "Brian",
+		},
+		Customer{
+			Id:   "1004",
+			Name: "Brian",
+		},
+	}
+
+	for _, c := range customers {
+		err := cs.Set(c).Run()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+	res := &[]Customer{}
+	err := cs.Where(Eq("Name", "Brian")).Read(res).Run()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := []string{"1004", "1003", "1001"}
+	if len(*res) != len(expected) {
+		t.Fatal("Expected", len(*res), " results, got", len(*res))
+	}
+	for i, id := range expected {
+		if (*res)[i].Id != id {
+			t.Fatal("Got result out of order. i:", i, "expected ID:", id, "actual ID:", (*res)[i].Id)
+		}
+	}
+}
+
+func TestClusteringOrderMultipl(t *testing.T) {
+	order := &ClusteringOrder{
+		Columns: []ClusteringOrderColumn{
+			ClusteringOrderColumn{
+				Ascending: false,
+				Column:    "Tag",
+			},
+			ClusteringOrderColumn{
+				Ascending: false,
+				Column:    "Id",
+			},
+		},
+	}
+	options := Options{Order: order}
+	name := "customer_by_name2"
+	cs := ns.Table(name, Customer2{}, Keys{
+		PartitionKeys:     []string{"Name"},
+		ClusteringColumns: []string{"Tag", "Id"},
+	}).WithOptions(options)
+	createIf(cs, t)
+
+	customers := []Customer2{
+		Customer2{
+			Id:   "1001",
+			Name: "Brian",
+			Tag:  "B",
+		},
+		Customer2{
+			Id:   "1002",
+			Name: "Adam",
+			Tag:  "A",
+		},
+		Customer2{
+			Id:   "1003",
+			Name: "Brian",
+			Tag:  "A",
+		},
+		Customer2{
+			Id:   "1004",
+			Name: "Brian",
+			Tag:  "B",
+		},
+	}
+
+	for _, c := range customers {
+		err := cs.Set(c).Run()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+	res := &[]Customer2{}
+	err := cs.Where(Eq("Name", "Brian")).Read(res).Run()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := []struct {
+		Tag string
+		Id  string
+	}{
+		{"B", "1004"},
+		{"B", "1001"},
+		{"A", "1003"},
+	}
+	if len(*res) != len(expected) {
+		t.Fatal("Expected", len(*res), " results, got", len(*res))
+	}
+	for i, e := range expected {
+		result := (*res)[i]
+		if result.Id != e.Id || result.Tag != e.Tag {
+			t.Fatal("Got result out of order. i:", i, "expected ID:", e.Id, "actual ID:", result.Id, "expected tag:", e.Tag, "actual tag:", result.Tag)
+		}
+	}
+}
+
 func TestCreateStatement(t *testing.T) {
 	cs := ns.Table("something", Customer{}, Keys{
 		PartitionKeys: []string{"Id", "Name"},
@@ -100,36 +234,6 @@ func TestCreateStatementWithClusteringOrder(t *testing.T) {
 		t.Fatal(str)
 	}
 	if !strings.Contains(str, "WITH CLUSTERING ORDER BY (Name DESC)") {
-		t.Fatal(str)
-	}
-}
-
-func TestCreateStatementWithMultipleClusteringOrder(t *testing.T) {
-	order := &ClusteringOrder{
-		Columns: []ClusteringOrderColumn{
-			ClusteringOrderColumn{
-				Ascending: false,
-				Column:    "Name",
-			},
-			ClusteringOrderColumn{
-				Ascending: true,
-				Column:    "Tag",
-			},
-		},
-	}
-	cs := ns.Table("something", Customer2{}, Keys{
-		PartitionKeys:     []string{"Id"},
-		ClusteringColumns: []string{"Name", "Tag"},
-	})
-	opts := Options{Order: order}
-	str, err := cs.WithOptions(opts).CreateStatement()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !strings.Contains(str, "something") {
-		t.Fatal(str)
-	}
-	if !strings.Contains(str, "WITH CLUSTERING ORDER BY (Name DESC, Tag ASC)") {
 		t.Fatal(str)
 	}
 }
