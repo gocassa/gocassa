6da371e0169d32446f67b05ce067a3296dbfb290
Janos Dobronszki <dobronszki@gmail.com>
2015-10-14 14:36:14 +0100

    Comment edits

--- options.go
+++ options.go
@@ -27,13 +27,14 @@ type Options struct {
 	TTL time.Duration
 	// Limit query result set
 	Limit int
-	// TableName
+	// TableName overrides the default internal table name. When naming a table 'users' the internal table name becomes 'users_someTableSpecificMetaInformation'.
 	TableName string
 	// ClusteringOrder specifies the clustering order during table creation. If empty, it is omitted and the defaults are used.
 	ClusteringOrder []ClusteringOrderColumn
-	// Indicates if allow filtering should be appeneded at the end of the query
+	// Indicates if allow filtering should be appended at the end of the query
 	AllowFiltering bool
-	Select         []string
+	// Select allows you to do partial reads, ie. retrieve only a subset of fields
+	Select []string
 }
 
 // Returns a new Options which is a right biased merge of the two initial Options.
